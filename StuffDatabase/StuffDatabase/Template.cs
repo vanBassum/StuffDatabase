76b190f7305cab9ede94b533449648063e18a8f6
Bas Visscher <bas>
2020-06-06 21:09:44 +0200

    Commit before trying a different approch

--- StuffDatabase/StuffDatabase/Template.cs
+++ StuffDatabase/StuffDatabase/Template.cs
@@ -2,6 +2,7 @@
 using System.Drawing.Imaging;
 using System.IO;
 using System.Reflection;
+using System.Text.RegularExpressions;
 
 namespace StuffDatabase
 {
@@ -19,42 +20,51 @@ namespace StuffDatabase
             File = file;
         }
 
-        public DYMO.Label.Framework.ILabel GetLabel(T chem)
+        public DYMO.Label.Framework.ILabel GetLabel(T obj)
         {
             DYMO.Label.Framework.ILabel label = DYMO.Label.Framework.DieCutLabel.Open(File);
 
-            foreach (DYMO.Label.Framework.ILabelObject obj in label.Objects)
+            foreach (DYMO.Label.Framework.ILabelObject labelObj in label.Objects)
             {
-                string name = obj.Name;
+                string name = labelObj.Name;
+                Match m = Regex.Match(name, @"([^_ \r\n]+)");
 
-                PropertyInfo propInfo = typeof(T).GetProperty(name);
-
-                if (propInfo != null)
+                if (m.Success)
                 {
-                    object prop = propInfo.GetValue(chem);
-
-                    switch (prop)
-                    {
-                        case string v:
-                            label.SetObjectText(name, v);
-                            break;
-                        case Image i:
-                            label.SetImagePngData("GHSimg", ImgToStream(i, ImageFormat.Png));
-                            break;
-                    }
+                    PropertyInfo propInfo = typeof(T).GetProperty(m.Groups[1].Value);
+                    if (propInfo != null)
+                        label.FillLabelObject(name, propInfo.GetValue(obj));
                 }
 
             }
             return label;
         }
 
-        Stream ImgToStream(Image image, ImageFormat format)
+        public DYMO.Label.Framework.ILabel GetLabel(T[] objs)
         {
-            var stream = new System.IO.MemoryStream();
-            image.Save(stream, format);
-            stream.Position = 0;
-            return stream;
+            DYMO.Label.Framework.ILabel label = DYMO.Label.Framework.DieCutLabel.Open(File);
+
+            foreach (DYMO.Label.Framework.ILabelObject labelObj in label.Objects)
+            {
+                string name = labelObj.Name;
+
+                Match m = Regex.Match(name, @"(.+?)_(\d+)");
+
+                if (m.Success)
+                {
+                    PropertyInfo propInfo = typeof(T).GetProperty(m.Groups[1].Value);
+                    int ind = int.Parse(m.Groups[2].Value);
+                    if (propInfo != null)
+                        label.FillLabelObject(name, propInfo.GetValue(objs[ind]));
+                }
+            }
+            return label;
         }
+
+
+
+
+        
 
 
         public override string ToString()
@@ -62,6 +72,31 @@ namespace StuffDatabase
             return Path.GetFileNameWithoutExtension(File);
         }
 
+    }
+
+    public static class DymoExt
+    {
+        public static void FillLabelObject(this DYMO.Label.Framework.ILabel label, string name, object value)
+        {
+            switch (value)
+            {
+                case string v:
+                    label.SetObjectText(name, v);
+                    break;
+
+                case Image i:
+                    label.SetImagePngData("GHSimg", ImgToStream(i, ImageFormat.Png));
+                    break;
+            }
+        }
+
+        static Stream ImgToStream(Image image, ImageFormat format)
+        {
+            var stream = new System.IO.MemoryStream();
+            image.Save(stream, format);
+            stream.Position = 0;
+            return stream;
+        }
     }
 
 }
