adbbd8d6f82732914bc85b09e6450cf303ad5741
Bas Visscher <bas@koolecontrols.nl>
2021-06-04 16:59:13 +0200

    Idea

--- StuffDatabase/Form1.cs
+++ StuffDatabase/Form1.cs
@@ -1,5 +1,6 @@
 ﻿using FRMLib;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Data;
 using System.Drawing;
@@ -66,6 +67,143 @@ namespace StuffDatabase
 
         
     }
+
+
+    public class Engine : ORMObject
+    { 
+    
+    }
+
+
+    public class Car : ORMObject
+    {
+        public string Name { get; set; }
+        public Engine Engine { get; set; }
+
+    }
+
+    public class MyDB : ORM
+    {
+        public DBList<Car> Cars { get; set; } = new DBList<Car>();
+    }
+
+
+    public class ORM
+    { 
+
+
+        void Save()
+        {
+            Dictionary<Type, Table> tables = new Dictionary<Type, Table>();
+
+            foreach(var pi in this.GetType().GetProperties())
+            {
+                object o = pi.GetValue(this);
+                if(o is DBList list)
+                {
+                    Do(tables, list);
+                }
+            }
+        }
+
+        void Do(Dictionary<Type, Table> tables, DBList list)
+        {
+            Table t;
+            if (!tables.TryGetValue(list.GenericType, out t))
+                tables.Add(list.GenericType, t = new Table());
+
+            foreach (ORMObject obj in list)
+            {
+                t.Insert(obj);
+
+                foreach (var pi in obj.GetType().GetProperties())
+                {
+                    object o = pi.GetValue(this);
+
+                    switch(o)
+                    {
+                        case DBList l:
+                            Do(tables, l);
+                            break;
+                        case ORMObject ob:
+                            Do(tables, ob);
+                            break;
+                    }
+                }
+            }
+        }
+
+
+        void Do(Dictionary<Type, Table> tables, ORMObject obj)
+        {
+            Table t;
+            if (!tables.TryGetValue(obj.GetType(), out t))
+                tables.Add(obj.GetType(), t = new Table());
+
+            t.Insert(obj);
+
+            foreach (var pi in obj.GetType().GetProperties())
+            {
+                object o = pi.GetValue(this);
+
+                switch (o)
+                {
+                    case DBList l:
+                        Do(tables, l);
+                        break;
+                    case ORMObject ob:
+                        Do(tables, ob);
+                        break;
+                }
+            }
+        }
+    }
+
+
+    public interface DBList : IEnumerable
+    {
+        Type GenericType { get; }
+    }
+
+
+    public class DBList<T> : DBList where T : ORMObject
+    {
+        List<T> list = new List<T>();
+        public Type GenericType { get => typeof(T); }
+
+        public IEnumerator GetEnumerator()
+        {
+            return list.GetEnumerator();
+        }
+    }
+
+
+    public class Table
+    {
+        List<ORMObject> objects = new List<ORMObject>();
+        public void Insert(ORMObject obj)
+        {
+            if (obj.id < 0)
+            {
+                obj.id = objects.Count();
+                objects.Add(obj);
+            }
+        }
+    }
+    /*
+    public class Table<T> : Table where T : ORMObject
+    {
+    }
+    */
+    public class ORMObject
+    {
+        public int id = -1;
+    }
+
+
+
+
+
 
 
 
